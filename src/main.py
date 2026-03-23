1674b7b0bcc21a311df92fad0c926b943873a199
manuel1507 <42934442+manuel1507@users.noreply.github.com>
2026-03-23 13:31:56 +0000

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -91,7 +91,8 @@ def main():
                 "text": clean_text(text[:9000]),
             }     
             )
-            print(clean_text(item.get("title", "")))
+            print(clean_text(item.get("title", "")) + url)
+            
         except Exception:
             continue
 
